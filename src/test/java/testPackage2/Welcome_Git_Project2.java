482262d62b69deaf0bd0f529a0f35f58deb95ebd
Veeraveni <18h41a0506cse@gmail.com>
2023-03-10 10:53:06 +0530

    Creating TestGitProject2

---
+++ src/test/java/testPackage2/Welcome_Git_Project2.java
@@ -0,0 +1,11 @@
+package testPackage2;
+
+public class Welcome_Git_Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to gitHub,jenkins and maven.");
+		// TODO Auto-generated method stub
+
+	}
+
+}
