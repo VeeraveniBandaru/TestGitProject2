03e42374247cc827ca2855f86bde51c05f80909f
Veeraveni <18h41a0506cse@gmail.com>
2023-03-10 11:21:28 +0530

    Adding NumberTest.java

---
+++ src/test/java/testPackage2/NumberTest.java
@@ -0,0 +1,9 @@
+package testPackage2;
+
+public class NumberTest {
+	public static void main(String[] args) {
+		System.out.println("This is number test code area......");
+		// TODO Auto-generated method stub
+
+	}
+}
